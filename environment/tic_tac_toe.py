2bb7d356875acb0c90df81f1eeea43c9bb574351
mabaoer <651540377@qq.com>
2022-08-15 17:55:12 +0800

    基础环境搭建完毕，训练过程有点没想清楚

--- environment/tic_tac_toe.py
+++ environment/tic_tac_toe.py
@@ -1,28 +1,38 @@
+import time
+
 import gym
 import numpy as np
 from gym.envs.classic_control import rendering
 
 
 class TicTacToeEnv(gym.Env):
-    def __init__(self, rows_cols=3):
-        self.rows_cols = rows_cols  # 井字棋的行列数
+    def __init__(self, cfg):
+        self.rows_cols = cfg.rows_cols  # 井字棋的行列数
         self.board_size = self.rows_cols * self.rows_cols
         self.state = np.zeros([self.rows_cols, self.rows_cols])
         self.state_unused = [(i, j) for i in range(self.rows_cols) for j in range(self.rows_cols)]
         self.winner = None
-        self.line_size = 100
+        self.line_size = cfg.line_size
         width, height = self.rows_cols * self.line_size, self.rows_cols * self.line_size
         self.viewer = rendering.Viewer(width, height)
+        self.current_action = None
+        self.count = -1
+        self.render_times = cfg.render_times
 
     def reset(self):
         self.state = np.zeros([self.rows_cols, self.rows_cols])
         self.state_unused = [(i, j) for i in range(self.rows_cols) for j in range(self.rows_cols)]
         self.winner = None
-        self.viewer.geoms.clear()  # 清空画板中需要绘制的元素
-        self.viewer.onetime_geoms.clear()
+        if self.count % self.render_times == 1:
+            time.sleep(1)
+            self.viewer.geoms.clear()  # 清空画板中需要绘制的元素
+            self.viewer.onetime_geoms.clear()
+        self.count += 1
 
     def step(self, action):
         # 动作的格式：action = {'mark':'circle'/'cross', 'pos':(x,y)}# 产生状态
+        self.current_action = action
+        print(action)
         x = action['pos'][0]
         y = action['pos'][1]
         if action['mark'] == 'blue':
@@ -72,34 +82,36 @@ class TicTacToeEnv(gym.Env):
         return False
 
     def render(self, mode='human'):
-        self.line_size = 100
-        # 画分隔线
-        for i in range(self.rows_cols - 1):
-            line1 = rendering.Line((0, self.line_size * (i + 1)),
-                                   (self.line_size * self.rows_cols, self.line_size * (i + 1)))
-            line2 = rendering.Line((self.line_size * (i + 1), 0),
-                                   (self.line_size * (i + 1), self.line_size * self.rows_cols))
-            line1.set_color(0, 0, 0)
-            line2.set_color(0, 0, 0)
-            # 将绘画元素添加至画板中
-            self.viewer.add_geom(line1)
-            self.viewer.add_geom(line2)
-        # 根据self.state画占位符
-        for i in range(self.rows_cols):
-            for j in range(self.rows_cols):
-                if self.state[i][j] == 1:
-                    circle = rendering.make_circle(self.line_size / 3)  # 画直径为30的圆
-                    circle.set_color(135 / 255, 206 / 255, 250 / 255)  # mark = blue
-                    move = rendering.Transform(translation=(
-                    i * self.line_size + self.line_size / 2, j * self.line_size + self.line_size / 2))  # 创建平移操作
-                    circle.add_attr(move)  # 将平移操作添加至圆的属性中
-                    self.viewer.add_geom(circle)  # 将圆添加至画板中
-                if self.state[i][j] == -1:
-                    circle = rendering.make_circle(self.line_size / 3)
-                    circle.set_color(255 / 255, 182 / 255, 193 / 255)  # mark = red
-                    move = rendering.Transform(
-                        translation=(i * self.line_size + self.line_size / 2, j * self.line_size + self.line_size / 2))
-                    circle.add_attr(move)
-                    self.viewer.add_geom(circle)
+        if self.count % self.render_times == 0:
+            for i in range(self.rows_cols - 1):
+                line1 = rendering.Line((0, self.line_size * (i + 1)),
+                                       (self.line_size * self.rows_cols, self.line_size * (i + 1)))
+                line2 = rendering.Line((self.line_size * (i + 1), 0),
+                                       (self.line_size * (i + 1), self.line_size * self.rows_cols))
+                line1.set_color(0, 0, 0)
+                line2.set_color(0, 0, 0)
+                # 将绘画元素添加至画板中
+                self.viewer.add_geom(line1)
+                self.viewer.add_geom(line2)
+            time.sleep(0.1)
+            # 根据self.state画占位符
+            i, j = self.current_action['pos'][0], self.current_action['pos'][1]
+            if self.state[i][j] == 1:
+                circle = rendering.make_circle(self.line_size / 3)  # 画直径为30的圆
+                circle.set_color(135 / 255, 206 / 255, 250 / 255)  # mark = blue
+                move = rendering.Transform(
+                    translation=(i * self.line_size + self.line_size / 2,
+                                 j * self.line_size + self.line_size / 2))  # 创建平移操作
+                circle.add_attr(move)  # 将平移操作添加至圆的属性中
+                self.viewer.add_geom(circle)  # 将圆添加至画板中
+
+            if self.state[i][j] == -1:
+                circle = rendering.make_circle(self.line_size / 3)
+                circle.set_color(255 / 255, 182 / 255, 193 / 255)  # mark = red
+                move = rendering.Transform(
+                    translation=(i * self.line_size + self.line_size / 2,
+                                 j * self.line_size + self.line_size / 2))
+                circle.add_attr(move)
+                self.viewer.add_geom(circle)
 
         return self.viewer.render(return_rgb_array=mode == 'rgb_array')
--- main.py
+++ main.py
@@ -1,16 +1,43 @@
-import gym
 import random
 import time
 from environment.tic_tac_toe import TicTacToeEnv
+from agent.qlearning import QLearning
+import os
+
+curr_path = os.path.dirname(os.path.abspath(__file__))  # 当前文件所在绝对路径
 
 
-def randomAction(env_, mark):  # 随机选择未占位的格子动作
-    action_pos = random.choice(env_.state_unused)
-    action = {'mark': mark, 'pos': action_pos}
-    return action
+class Config:
+    def __init__(self):
+        # 环境超参数
+        self.env_name = "tic_tac_toe"
+        self.rows_cols = 3  # 井字棋的行列数
+        self.line_size = 100  # 井字棋每个各自的长宽
+        self.render_times = 10  # 每隔多久渲染一次
+
+        ################################## 环境超参数 ###################################
+        self.algo_name = 'Q-learning'  # 算法名称
+        self.seed = 10  # 随机种子，置0则不设置随机种子
+        self.train_eps = 400  # 训练的回合数
+        self.test_eps = 30  # 测试的回合数
+        ################################################################################
+
+        ################################## 算法超参数 ###################################
+        self.gamma = 0.90  # 强化学习中的折扣因子
+        self.epsilon_start = 0.95  # e-greedy策略中初始epsilon
+        self.epsilon_end = 0.01  # e-greedy策略中的终止epsilon
+        self.epsilon_decay = 300  # e-greedy策略中epsilon的衰减率
+        self.lr = 0.1  # 学习率
+        ################################################################################
+
+        ################################# 保存结果相关参数 ################################
+        self.result_path = curr_path + "/outputs/" + self.env_name + '/results/'  # 保存结果的路径
+        self.model_path = curr_path + "/outputs/" + self.env_name + '/models/'  # 保存模型的路径
+        self.save = True  # 是否保存图片
+        ################################################################################
 
 
-def randomFirst():
+def random_first():
     if random.random() > 0.5:  # 随机先后手
         first_, second_ = 'blue', 'red'
     else:
@@ -18,29 +45,27 @@ def randomFirst():
     return first_, second_
 
 
-env = TicTacToeEnv(rows_cols=5)
-env.reset()  # 在第一次step前要先重置环境 不然会报错
-first, second = randomFirst()
-while True:
-    # 先手行动
-    action = randomAction(env, first)
-    state, reward, done, info = env.step(action)
-    env.render()
-    time.sleep(0.1)
-    if done:
-        time.sleep(1)
-        env.reset()
+if __name__ == "__main__":
+    cfg = Config()
+    env = TicTacToeEnv(cfg)
+    agent = QLearning(cfg)
+    env.reset()  # 在第一次step前要先重置环境 不然会报错
+    first, second = random_first()
+    while True:
+        # 先手行动
+        action = agent.choose_action(env.state, first, env.state_unused)
+        state, reward, done, info = env.step(action)
+        env.render(action)
+        if done:
+            env.reset()
+            first, second = random_first()
+            continue
+
+        # 后手行动
+        action = agent.choose_action(env.state, second, env.state_unused)
+        state, reward, done, info = env.step(action)
         env.render()
-        first, second = randomFirst()
-        continue
-    # 后手行动
-    action = randomAction(env, second)
-    state, reward, done, info = env.step(action)
-    env.render()
-    time.sleep(0.1)
-    if done:
-        time.sleep(1)
-        env.reset()
-        env.render()
-        first, second = randomFirst()
-        continue
+        if done:
+            env.reset()
+            first, second = random_first()
+            continue
